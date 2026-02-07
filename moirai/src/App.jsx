c153cd1455613fcb9199e8d45b78b3b8eaf57de2
Evrenefeb <141721390+Evrenefeb@users.noreply.github.com>
2026-02-07 17:28:37 +0300

    Devenv setup

--- moirai/src/App.jsx
+++ moirai/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Accordion } from 'reactstrap';
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -27,6 +29,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+
+        
       </p>
     </>
   )
