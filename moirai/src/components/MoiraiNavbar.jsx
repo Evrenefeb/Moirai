0969290ba719e3708d16fc17ec19cd4e7d87762b
fatihb4rat-jpg <fatihb4rat@gmail.com>
2026-02-08 15:24:13 +0300

    directory update

---
+++ moirai/src/components/MoiraiNavbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import logo from '../assets/Moirai_logo.png'
+import './MoiraiNavbar.css';
+
+const MoiraiNavbar = () => {
+  return (
+    <nav className="navbar">
+      {/* Left: Logo */}
+      <a href="/" className="navbar-brand">
+        <span>Moirai</span>
+        
+      </a>
+
+      {/* Right: Links */}
+      <div className="navbar-links">
+        <a href="#about">About Us</a>
+        <a href="https://https://github.com/Evrenefeb/Moirai.com/" target="_blank" rel="noreferrer">
+          Docs ↗
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default MoiraiNavbar;
