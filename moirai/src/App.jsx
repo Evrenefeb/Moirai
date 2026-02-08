0f8e2cc2aed79d5f76a5550a77a51f5ca60500be
fatihb4rat-jpg <fatihb4rat@gmail.com>
2026-02-08 15:30:05 +0300

    directory update

--- moirai/src/App.jsx
+++ moirai/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './color_palette.css'
 import './App.css'
-import Navbar from './components/MoiraiNavbar.jsx'
+import Navbar from './components/MoiraiNavbar/MoiraiNavbar.jsx'
 
 function App() {
   return (
