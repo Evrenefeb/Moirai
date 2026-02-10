438364b7561208d7c179352bccae7b4b718da942
Evrenefeb <141721390+Evrenefeb@users.noreply.github.com>
2026-02-10 14:56:47 +0300

    Added DoughtnutChart component

--- moirai/src/App.jsx
+++ moirai/src/App.jsx
@@ -2,6 +2,7 @@ import './color_palette.css'
 import './App.css'
 import Navbar from './components/MoiraiNavbar/MoiraiNavbar.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import DoughnutChart from './components/DoughtnutChart/DoughtnutChart.jsx'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
     <Navbar />
 
+    <DoughnutChart />
 
     <Footer />
     </>
